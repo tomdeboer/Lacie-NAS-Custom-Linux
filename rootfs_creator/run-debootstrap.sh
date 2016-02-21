a7df2863e5b9622e58639e82348bf680553f9495
Tom Boer <tom@tomdeboer.nl>
2016-02-21 20:30:17 +0100

    Added extfs support via fuse

--- rootfs_creator/run-debootstrap.sh
+++ rootfs_creator/run-debootstrap.sh
@@ -4,4 +4,4 @@
 # you are ready do run debootstrap. This will install the base
 # system including the necessary packages to make it "NAS-worthy".
 
-qemu-debootstrap --arch=armel --variant=minbase --verbose --foreign --include=nano,ntpdate,wget,net-tools,iputils-ping jessie /files/rootfs "http://ftp.nl.debian.org/debian/"
+qemu-debootstrap --arch=armel --variant=minbase --verbose --foreign --include=exfat-fuse,rsync,nano,ntpdate,wget,net-tools,iputils-ping jessie /files/rootfs "http://ftp.nl.debian.org/debian/"
